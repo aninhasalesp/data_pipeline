0d7f183057a429af7878d31425c2107397bd8081
Ana Paula Gonzaga <aninha.sales58@hotmail.com>
2023-01-06 23:46:32 -0300

    - add states enum  - add main pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,33 @@
 import pandas as pd
 
+from utils.enum import estados_uf
+
+
 def run(censo_csv_path: str, rouanet_csv_path: str) -> str:
-    pass
+    """Main pipeline"""
+    censo_dataframe = pd.read_csv(censo_csv_path)
+    rouanet_dataframe = pd.read_csv(rouanet_csv_path)
+
+    merged_dataframes = pd.merge(
+        censo_dataframe, 
+        rouanet_dataframe,
+        left_on='codigo', 
+        right_on='estado_ibge', 
+    )
+    merged_dataframes.set_index(["estado_ibge", "valor_em_reais"])
+    removed_duplicated_dataframe = merged_dataframes.drop_duplicates(
+        subset=["estado_ibge", "valor_em_reais"]
+    )
+    
+    cleared_dataframe = removed_duplicated_dataframe.drop(
+    removed_duplicated_dataframe[
+            (
+                removed_duplicated_dataframe.valor_em_reais == 0
+            ) | (
+                removed_duplicated_dataframe.quantidade == 0
+            )
+        ].index
+    )
+
+    result_dataframe = cleared_dataframe.replace({"estado": estados_uf})
+    return result_dataframe.to_csv()
