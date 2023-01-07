348b21d12965a5a0d851faf505cf26765b22071f
Ana Paula Gonzaga <aninha.sales58@hotmail.com>
2023-01-06 23:23:47 -0300

    - add pyproject.toml  - start tests  - add tests resources

---
+++ tests/test_pipeline.py
@@ -0,0 +1,15 @@
+import sys
+from pathlib import Path
+
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+
+from pipeline import run as run_pipeline
+
+
+def test_pipeline() -> None:
+    with open("resources/dados_tratados.csv", "r") as dados_tratados:
+        assert run_pipeline(
+            "resources/censo_estado.csv",
+            "resources/rouanet.csv"
+        ) == dados_tratados.read()
